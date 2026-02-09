db159e0546e73f1bc0fec43d0d2a81654ccb268a
Arto Bendiken <arto@bendiken.net>
2026-02-09 05:41:57 -0800

    Outline the Rust library.

---
+++ rust/src/lib.rs
@@ -0,0 +1,11 @@
+// This is free and unencumbered software released into the public domain.
+
+//! A modern evolution on flow-based programming (FBP).
+
+#![no_std]
+#![forbid(unsafe_code)]
+
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
