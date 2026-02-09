75780ca7ea967448a05775927361df4c5fd7f873
Arto Bendiken <arto@bendiken.net>
2026-02-09 06:12:43 -0800

    Outline the Python library.

---
+++ python/src/flux/theory/__init__.py
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+"""A modern evolution on flow-based programming (FBP)."""
+
+import sys
+
+assert sys.version_info >= (3, 10), "Flux Theory requires Python 3.10+"
+
+from ._version import __version__, __version_tuple__
